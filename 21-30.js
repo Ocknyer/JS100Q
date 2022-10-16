70251a8a39a7ee753ea5e5d32ec24db436f40fd9
Ocknyer <kdu4045@naver.com>
2022-10-16 20:04:39 +0900

    js 100제 문제 풀이

--- 21-30.js
+++ 21-30.js
@@ -12,3 +12,34 @@ function 원너비 () {
     return r * r * 3.14;
 }
 원너비()
+
+// 26. 행성 문제2
+// 우리 태양계를 이루는 행성은 수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성이 있습니다.
+// 이 행성들의 영어 이름은 Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune입니다.
+
+// 행성의 한글 이름을 입력하면 영어 이름을 반환하는 프로그램을 만들어 주세요.
+
+let 행성 = prompt('행성의 한글 이름을 입력하세요.');
+if (행성 = '수성') {
+    console.log('Mercury');
+} else if (행성 = '금성') {
+    console.log('Venus');
+} else if (행성 = '지구') {
+    console.log('Earth');
+} else if (행성 = '화성') {
+    console.log('Mars');
+} else if (행성 = '목성') {
+    console.log('Jupiter');
+} else if (행성 = '토성') {
+    console.log('Saturn');
+} else if (행성 = '천왕성') {
+    console.log('Uranus');
+} else if (행성 = '해왕성') {
+    console.log('Neptune');
+}
+
+// 27. 객체 만들기
+// 첫번째 입력에서는 학생의 이름이 공백으로 구분되어 입력되고, 두번째에는 그 학생의 수학 점수가 공백으로 구분되어 주어집니다.
+
+// 두 개를 합쳐 학생의 이름이 key이고 value가 수학 점수인 객체를 출력해주세요.
+
