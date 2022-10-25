bf10caabab4a74cd9d483034d5bee22a55519890
Ocknyer <kdu4045@naver.com>
2022-10-25 09:09:33 +0900

    30 ~ 35번 풀이

--- 21-30.js
+++ 21-30.js
@@ -99,8 +99,7 @@ if (alphabet === alphabet.toUpperCase()) {
 
 // 첫번째 입력에서는 문자열이 입력되고, 두번째에는 찾을 문자가 입력되어야 합니다. 그 문자가 시작하는 index를 반환하는 프로그램을 만들어 주세요.
 
-let 외부문자열 = prompt('문자열을 입력하세요.')
-let 내부문자열 = prompt('내부 문자열을 입력하세요.')
-if (외부문자열.includes(내부문자열)) {
-    console.log
-}
+let outerStr = prompt('문자열을 입력하세요.');
+let innerStr = prompt('내부 문자열을 입력하세요.');
+
+console.log(outerStr.indexOf(innerStr));
